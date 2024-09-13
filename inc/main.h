0f92a78d935477747c4ad877e5b75976a0512824
Amine Behloul <behloulamine30@gmail.com>
2024-09-13 23:17:03 +0200

    Add timer library

--- inc/main.h
+++ inc/main.h
@@ -3,11 +3,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
 #define BUTTON1 16
 #define BUTTON2 17
 #define BUTTON3 18
 #define BUTTON4 19
 
+extern volatile bool alarm_fired;
+extern void alarm_in_us(uint32_t delay_us);
 
 #endif
